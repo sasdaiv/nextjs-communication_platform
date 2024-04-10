a35ab7a7a540d769d34bbea7ccfb30c8d0951ef4
sasdaiv <sasdaiv@gmail.com>
2024-04-10 14:38:52 +0300

    clerk auth and pages

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="flex size-full flex-col gap-10 text-white">
+      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h2></h2>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import MobileNav from "@/components/MobileNav";
+import { UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          alt="logo"
+          width={32}
+          height={32}
+          className="max-sm:size-10"
+        />
+        <p className="text-[27px] font-extrabold text-white max-sm:hidden">
+          Logo
+        </p>
+      </Link>
+
+      <div className="flex-between gap-5">
+        <UserButton />
+        <MobileNav />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
